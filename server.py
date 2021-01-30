634f8e81bb7636cb83bb76ce71beefdd7b7acbb4
pik94 <pinchuk.ilia.work@gmail.com>
2021-01-30 17:58:40 +0300

    Production settings
    1. Arguments such as debug, host, port, storage_path, log_file, chunk_size and delay are read via argparse
    2. Function set_logger_settings is used for settings of logger
    3. Settings are set via a special class ServerSettings

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import logging
 from pathlib import Path
@@ -7,10 +8,9 @@ from aiohttp import web
 import aiofiles
 
 
-from settings import CHUNK_SIZE, INTERVAL_SECS
+from settings import set_logger_settings, ServerSettings as sts
 
 
-logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__file__)
 
 
@@ -35,11 +35,16 @@ class Archiver:
         return self
 
     async def __aexit__(self, exc_type, exc, exc_tb):
+        """
+        kill process if something wrong happens.
+        """
+
         if exc:
             self._logger.warning('Downloading data from a pipe was '
                                  'interrupted')
             try:
-                self._proc.kill()
+                self.process.kill()
+                await self.process.communicate()
             except Exception as e:
                 logger.error(f'Cannot kill an archiving process: {type(e)}')
             finally:
@@ -51,9 +56,10 @@ class Archiver:
         return self._proc
 
     async def read(self,
-                   chunk_size: Optional[int] = CHUNK_SIZE * 1024) -> bytes:
+                   chunk_size: Optional[int] = sts.CHUNK_SIZE * 1024) -> bytes:
         """
-        Read chunked data from a stdin
+        Read chunked data from a stdin.
+        If a process return non-zero code, RuntimeError exception is raised.
         :param chunk_size: in kilobytes
         :return:
         """
@@ -74,7 +80,7 @@ async def archive(request: web.Request) -> web.StreamResponse:
     if not archive_hash:
         raise web.HTTPNotFound(reason='Archive does not exist or was deleted.')
 
-    archive_path = Path.cwd() / f'test_photos/{archive_hash}'
+    archive_path = Path(f'{sts.STORAGE_PATH}') / archive_hash
     if not archive_path.exists():
         raise web.HTTPNotFound(reason='Archive does not exist or was deleted')
 
@@ -89,13 +95,13 @@ async def archive(request: web.Request) -> web.StreamResponse:
     try:
         async with Archiver(archive_path, logger) as archiver:
             while True:
-                out = await archiver.read()
+                out = await archiver.read(sts.CHUNK_SIZE * 1024)
                 await stream_response.write(out)
 
                 if archiver.process.stdout.at_eof():
                     await stream_response.write_eof()
                     break
-                await asyncio.sleep(INTERVAL_SECS)
+                await asyncio.sleep(sts.DELAY)
     except asyncio.CancelledError:
         raise
     except RuntimeError:
@@ -113,10 +119,90 @@ async def handle_index_page(request: web.Request) -> web.Response:
     return web.Response(text=index_contents, content_type='text/html')
 
 
-if __name__ == '__main__':
+class WebServer:
+    def __init__(self):
+        self._app = web.Application()
+
+    def run(self,
+            debug: Optional[bool] = True,
+            host: Optional[str] = 'localhost',
+            port: Optional[int] = 8080):
+        routes = [web.get('/archive/{archive_hash}/', archive)]
+        if debug:
+            routes.append(web.get('/', handle_index_page))
+
+        self._app.add_routes(routes)
+        web.run_app(self._app, host=host, port=port)
+
+
+def main(args: argparse.Namespace):
+    debug = args.debug
+    host = args.host
+    port = args.port
+
+    sts.STORAGE_PATH = args.storage_path
+    sts.DELAY = args.delay
+    sts.CHUNK_SIZE = args.chunk_size
+
+    routes = [web.get('/archive/{archive_hash}/', archive)]
+    if debug:
+        routes.append(web.get('/', handle_index_page))
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    set_logger_settings(args.log_file, level)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
     ])
-    web.run_app(app)
+    web.run_app(app, host=host, port=port)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',
+                        '--debug',
+                        required=False,
+                        action='store_true',
+                        default=False,
+                        help='If set you can use an index page for debugging.')
+    parser.add_argument('--host',
+                        type=str,
+                        required=False,
+                        default='localhost',
+                        help='A host where a server will be deployed. '
+                             'By default, "localhost"')
+    parser.add_argument('--port',
+                        type=int,
+                        required=False,
+                        default=8080,
+                        help='A port where a server will be deployed. '
+                             'By default, 8080')
+    parser.add_argument('--storage_path',
+                        type=str,
+                        required=False,
+                        default='test_photos',
+                        help='A directory with all photo. '
+                             'By default, ./test_photos')
+    parser.add_argument('--log_file',
+                        type=str,
+                        required=False,
+                        default='archive.log',
+                        help='A path to a log file. By default, ./archive.log')
+    parser.add_argument('--chunk_size',
+                        type=int,
+                        required=False,
+                        default=100,
+                        help='A chunk size (in kilobytes) for chunks '
+                             'which are sent to a client. By default, 100.')
+    parser.add_argument('--delay',
+                        type=int,
+                        required=False,
+                        default=1,
+                        help='Delay (in seconds) between sending chunk to '
+                             'a client. By default, 1 second.')
+    args = parser.parse_args()
+    main(args)
--- settings.py
+++ settings.py
@@ -1,5 +1,56 @@
-# An interval between sending a chunk of data to a client
-INTERVAL_SECS = 1
+import logging
+from logging.config import dictConfig
+from pathlib import Path
+from typing import NoReturn, Optional
 
-# How many data is sent to a clint (in Kb)
-CHUNK_SIZE = 100
+
+class ServerSettings:
+    # An interval between sending a chunk of data to a client
+    DELAY = 5
+
+    # How many data is sent to a clint (in Kb)
+    CHUNK_SIZE = 100
+
+    # A directory path to all photos
+    STORAGE_PATH = 'test_photos'
+
+
+def set_logger_settings(log_file: str,
+                        level: Optional[int] = logging.DEBUG) -> NoReturn:
+    log_file = Path(log_file)
+    log_file.parent.mkdir(exist_ok=True, parents=True)
+    config = dict(
+        version=1,
+        formatters={
+            # For files
+            'detailed': {
+                'format': '%(asctime)s %(levelname)-8s %(message)s'
+            },
+            # For the console
+            'console': {
+                'format': '%(asctime)s [%(levelname)s] %(message)s'}
+        },
+        handlers={
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': level,
+                'formatter': 'console',
+            },
+            'file': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'level': level,
+                'formatter': 'detailed',
+                'filename': log_file,
+                'mode': 'a',
+                'maxBytes': 1048576,  # 1 MB
+                # 'maxBytes': 10485760,  # 10 MB
+                'backupCount': 5
+            }
+        },
+        root={
+            'handlers': ['console', 'file'],
+            'level': level,
+        },
+        disable_existing_loggers=False
+    )
+    dictConfig(config)
